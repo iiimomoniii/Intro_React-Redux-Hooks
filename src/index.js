08d78a022c60166a7023c6f4cf6c8317b20e6ab1
Natdanai <natdanai_thaiyala@live.com>
2023-09-06 17:06:46 +0700

    prepare redux for project

--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Redux Begin
+//hoc
+import { Provider } from 'react-redux';
+//function for create store
+import {createStore, applyMiddleware} from 'redux';
+//print log
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+//reducers
+import reducers from './reducers/';
+//Redux End
+
+//prepared store
+const store = createStore(reducers, applyMiddleware(thunk, logger));
+
+//prepared ReduxApp
+const ReduxApp = ()=> {
+  return (
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  )
+}
+
+//replace from App to ReduxApp
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReduxApp />
   </React.StrictMode>
 );
 
