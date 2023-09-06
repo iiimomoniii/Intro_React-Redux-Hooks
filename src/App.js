ae0642281317f7bdca693d222410ee15a10d78bc
Natdanai <natdanai_thaiyala@live.com>
2023-09-06 17:39:39 +0700

    how to update value in reducer by dispatch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React,{useState} from "react";
 //add hook of react-redux
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import Box1 from './components/Box1';
 import Box2 from './components/Box2';
 import Box3 from './components/Box3';
+//import action and catch to appAction
+import * as appAction from './actions/app.action';
 export default function App() {
   
   //0.init appReducer
   //0.1 map appReducer in useSelector
   const appReducer = useSelector(({appReducer:appReducer})=>appReducer)
+  //0.2 call dispatch hook for use action by reducer
+  const dispatch = useDispatch();
 
   return (
     <div style={{padding:10}}>
+      <div style={{display:'flex', flexDirection:'row'}}>
+        {/*2. update add action in reducer by dispatch*/}
+        <button onClick={()=>{dispatch(appAction.add())}}>Add</button>
+        <button onClick={()=>{}}>REM</button>
+        <button onClick={()=>{}}>Clear</button>
+      </div>
       {/*access function count in appReducer*/}
-      <h1 >Codemobiles : {appReducer.count}</h1>
+      <h1>Codemobiles : {appReducer.count}</h1>
       <Box1/>
       <Box2/>
       <Box3/>
--- src/reducers/app.reducer.js
+++ src/reducers/app.reducer.js
@@ -1,13 +1,20 @@
 //(Reducer State in react cycle)
+import {APP_ACTION_ADD} from '../constants/app.constant'
 const initialState = {
-    count:10,
-    account : {username : "admin", password : "1234"}
-}
+  count: 0,
+  account: { username: "admin", password: "1234" },
+};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    //1 case add action
+    case APP_ACTION_ADD:
+      //update value when add action
+      //current count equal previous count plus 1
+      //...state equal clone account and set to new state
+      return { ...state, count: state.count + 1 };
 
-  default:
-    return state
+    default:
+      return state;
   }
-}
+};
